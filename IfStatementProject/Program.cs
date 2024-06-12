444d07367d2a0548237f5d39b7838f2c79a9e49b
chrisdeckard2 <christopherdaviddeckard@gmail.com>
2024-06-12 14:32:39 -0400

    Add project files.

---
+++ IfStatementProject/Program.cs
@@ -0,0 +1,33 @@
+﻿while (true)
+{
+
+
+    Console.WriteLine("Enter an integer: ");
+    string answer = Console.ReadLine();
+
+
+    int aNumber = Convert.ToInt32(answer);
+
+    if (aNumber == 0)
+    {
+        break;
+
+    }
+
+    if (aNumber % 2 == 0)
+    {
+
+        Console.WriteLine($"{aNumber} is even.");
+    }
+    else
+    {
+        Console.WriteLine($"{aNumber} is even.");
+
+    }
+}
+
+
+
+Console.WriteLine("Done...");
+
+
